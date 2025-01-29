c9a6fa29a2d345dac82fb693742e23a58380f9a0
linnet garcia <linnetgarcia00@gmail.com>
2025-01-28 19:54:54 -0600

    Logro: Se agregaron nuevas consultas de test de comparacion

--- Unidad 1/02-consultassimples.sql
+++ Unidad 1/02-consultassimples.sql
@@ -41,9 +41,16 @@ year (orderdate)as 'Año',
 MONTH(orderdate)as 'Mes', 
 day(orderdate) as 'Dia de la orden',
 CustomerID, EmployeeID from Orders;
+-- Filas Duplicadas (Distinct) Quita datos repetidos
+select * from Customers;
+-- Mostrar los paises en donde se tienen clientes 
+-- Mostrando pais solamente 
+Select distinct Country from Customers
+order by country
 
 -- Clausula where 
---Operadores relacionales (<,>,=,<=,>=, != o <>)
+--Operadores relacionales o test de comparacion (<,>,=,<=,>=, != o <>)
+
 select CustomerID, CompanyName, City, country
 from Customers 
 where CustomerID = 'BOLID';
@@ -84,3 +91,26 @@ ShippedDate as 'Fecha de envio ',
 CustomerID as 'Nombre'
 from Orders
 where year(OrderDate) = '1996'
+
+-- Mostrar todas las ordenes de compra donde la cantidad de productos comprados 
+-- sea mayor a 40
+select Quantity from [Order Details]
+where Quantity >= 40
+
+-- Mostrar el nombre completo del empleado, su numero de empleado, su fecha de nacimiento
+-- ciudad, fecha de contratación debe ser de aquellos que fueron contratados despues de 1993
+-- los resultados en sus encabezados deben ser mostrados en español
+select EmployeeID as 'Numero',
+(FirstName, + 'Nombre',  + LastName )  
+BirthDate as 'Fecha de nacimiento', City as 'Ciudad', HireDate as 'FechaContrato'
+from Employees
+where year(HireDate) >1993
+
+select EmployeeID as 'Numero',
+concat(FirstName,  ' ', LastName, '-', Title ) as [NombreCompleto]  
+BirthDate as 'Fecha de nacimiento', City as 'Ciudad', HireDate as 'FechaContrato'
+from Employees
+where year(HireDate) >1993
+-- Mostrar los empleados que no son dirigidos por el jefe 2
+select EmployeeID from Employees
+--Seleccionar los empleados que no tengan 
