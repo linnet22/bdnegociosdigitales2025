2cfe445e71e2e614e1245804e0b23d6340392df8
linnet garcia <linnetgarcia00@gmail.com>
2025-03-18 19:54:27 -0600

    Logro : Funciones de cadena, vistas y store procedure terminados

---
+++ Unidad2/07-transact-funciones-cadena.sql
@@ -0,0 +1,85 @@
+-- Funciones de cadena
+-- Las funciones de cadena permite manipular tipos de datos como varchar,nvarchar,char y nchar
+-- Funcion Len -> devuelve la longitud de una cadena 
+--Declaración de una variable
+
+
+DEclare @numero int;
+set @numero =10;
+print @numero
+-- Obtener el tamaño de la cadena almacenada en la variable texto
+
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+
+select LEN(@Texto ) AS [Longitud]
+
+select CompanyName, LEN(CompanyName) from Customers 
+
+-- Funcion left 
+-- Extrae un numero especifico de caracteres desde
+---el inicio de la cadena 
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+select LEFT(@Texto, 4) as Inicio
+-- Rigt 
+--Extrae un determinado de caracteres
+--del final de la cadena
+--eclare @Texto varchar (50) = 'Hola, Mundo!';
+select RIGHT(@Texto, 6) as Final
+
+-- Substring -> Esxtrae una parte de la cadena 
+-- cuenta desde la posicion indicada hasta el margen que agregas 
+--su recorrido o sea el numero de caracteres 
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+select SUBSTRING(@Texto,7,5) as 'Sub Cadena'
+
+-- replace -> Reemplaza una sub cadena por otra
+--REPLACE( string_expresion, string_pattern, String_replacement )
+
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+select replace(@Texto, 'Mundo', 'Amigo')
+
+-- CharIndex 
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+select CHARINDEX('Mundo', @Texto)
+
+-- UPPER cOMBIERTE UNA CADENA EN MAYUSCULAS 
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+select UPPER(@Texto) as Mayusculas
+
+--declare @Texto varchar (50) = 'Hola, Mundo!';
+Select CONCAT(
+			LEFT(@Texto,6),
+			UPPER(SUBSTRING(@Texto,7,5)),
+			RIGHT(@Texto,1)
+			) as TextoNuevo
+UPDATE Customers
+SET CompanyName = UPPER(CompanyName)
+where Country in('Mexico', 'Germany')
+
+select * from Customers
+
+
+--TRIM QUITA ESPACIOS EN BLANCO DE UNA CADENA
+--SELECT TRIM( '     test    ') AS Result;
+declare @Texto varchar (50) = 'Hola, Mundo!';
+SELECT TRIM(@Texto) as Result;
+
+-- CREar un store procedure para seleccionar todos los clientes 
+-- es un conjunto de instrucciones
+go
+create or alter procedure spu_mostrar_clientes 
+AS
+begin
+
+	select * from Customers;
+	end;
+	go
+-- Ejecutar un store transact
+exec spu_mostrar_clientes 
+
+
+------------------------------------
+select CompanyName, LEN(CompanyName) as 'Numero de Caracteres',
+LEFT(CompanyName, 4) as Inicio,
+ RIGHT(CompanyName, 6) as Final
+from Customers 
---
+++ Unidad2/09_EJERCICIO-PERIDOSTORE-PROCEDURE.sql
@@ -0,0 +1,69 @@
+--Realizar un pedido 
+-- Validar que el pedido noexista 
+--Validar que el cliente, el empleado y producto  exista 
+-- Validar la cantidad a vender tenga suficiente stock
+--Insertar el pedido y calcular el importe (Multiplicando el precio del 
+-- producto por cantidad vendida )
+--Actualizar el stock del producto (Restando el stockmenos la cantidad vendida )   
+create or alter procedure spu_realizar_pedido
+@numPedido int , @cliente int ,
+@repre int, @fab char(3),
+@producto char(5), @cantidad int
+ as
+ begin -- Validar que el pedido noexista 
+   if exists(select 1 from Pedidos where Num_Pedido = @numPedido)
+   begin 
+   print('El pedido ya existe ')
+   return
+   end
+
+		  if not exists(select 1 from Clientes where Num_Cli = @cliente) OR --Validar que el cliente, el empleado y producto  exista 
+			 not exists (select 1 from Representantes where Num_Empl = @repre)OR 
+			 not exists (select 1 from Productos where Id_fab = @fab and Id_producto = @producto)
+
+	   begin 
+	   print('Los DATOS no son validos')
+	   return
+	   end
+			if @cantidad <=0
+			BEGIN 
+			PRINT 'La cantidad no puede ser 0 o negativo '
+			return;
+			end 
+					declare @stockValido int 
+					select Stock from Productos where Id_fab =@fab and Id_producto =@producto
+						if @cantidad > @stockValido
+						begin 
+						print 'No hay suficiente stock'
+						return;
+						end 
+						declare @precio money 
+						declare @importe money 
+						select @precio=Precio from Productos where Id_fab = @fab and Id_producto = @producto
+						set @importe = @cantidad * @precio
+						begin try 
+						insert into Pedidos 
+					values (@numPedido,getDate (), @cliente,@repre,@fab,@producto,@cantidad,@importe)
+ update Productos
+ set Stock = Stock - @cantidad
+ where Id_fab = @fab and Id_producto =@producto;
+ end try
+ -- se inserto pedido 
+ begin catch
+ print 'Error al utilizar los datos'
+ return;
+ end catch
+ end;
+
+
+
+ exec spu_realizar_pedido 
+							@numPedido = 113069,
+							@cliente =1000000,
+							@repre =106, @fab = 'REI',
+							@producto = '2A44L', 
+							@cantidad =20
+
+
+reretes 
+ select * from Pedidos  
